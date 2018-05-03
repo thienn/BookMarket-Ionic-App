3de6f2088f6f8c8973614216c64a83c66e5ffe6e
thienn <thien.oslo@gmail.com>
2018-05-03 05:56:15 +0200

    Simple login & register provider for usage in App

--- src/providers/authorize/authorize.ts
+++ src/providers/authorize/authorize.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { AngularFirestore } from 'angularfire2/firestore';
+
 /*
   Generated class for the AuthorizeProvider provider.
 
@@ -10,8 +12,40 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthorizeProvider {
 
-  constructor(public http: HttpClient) {
+  public user = {
+    username: "",
+    password: ""
+  }
+
+  constructor(public http: HttpClient, private af: AngularFirestore) {
     console.log('Hello AuthorizeProvider Provider');
   }
+
+  // Similar setup on both, just using different call. Try / catch method for success & error if something goes wrong. Rest is handled by the Firestore module
+
+  // Login
+  loginUser() {
+    this.af.app.auth()
+      .signInWithEmailAndPassword(this.user.username, this.user.password)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
+  // Register
+  registerUser() {
+    this.af.app.auth()
+      .createUserWithEmailAndPassword(this.user.username, this.user.password)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
 
 }
