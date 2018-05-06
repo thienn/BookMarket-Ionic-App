e33101250ca214ad39ccb48a68361f80ea59d986
thienn <thien.oslo@gmail.com>
2018-05-06 20:23:17 +0200

    Design updates to detail page
    + Added the post and the comments in seperate cards for future use.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -27,15 +27,15 @@ export class LoginPage {
   }
   
   // Login
- loginUser() {
-  this.af.app.auth()
-  .signInWithEmailAndPassword(this.user.username, this.user.password)
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => {
-    console.error(error);
-  })
+  loginUser() {
+    this.af.app.auth()
+      .signInWithEmailAndPassword(this.user.username, this.user.password)
+      .then(response => {
+       console.log(response);
+      })
+      .catch(error => {
+      console.error(error);
+    })
 }
 
   // Register
