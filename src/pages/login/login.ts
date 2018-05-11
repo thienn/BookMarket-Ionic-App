e6b4aa99d8c92ca045e42d0cb81438875fda8707
thienn <thien.oslo@gmail.com>
2018-05-11 09:54:43 +0200

    Updates to login page
    + Some temporary style changes to see the input fields better
    + Added alert, loading and toast messages to improve the experience.
    + alert specifically with error messages in case of any problems with registering og logging in

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AuthorizeProvider } from '../../providers/authorize/authorize';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController, ToastController } from 'ionic-angular';
 import { AngularFirestore } from 'angularfire2/firestore';
 
 /**
@@ -23,30 +22,59 @@ export class LoginPage {
   }
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private af: AngularFirestore) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private af: AngularFirestore, private loadingCtrl: LoadingController, private alertCtrl: AlertController, private toast: ToastController) {
   }
   
   // Login
   loginUser() {
+    let loading = this.loadingCtrl.create({ content: "Logger inn..."});
+    loading.present();
+
   this.af.app.auth()
     .signInWithEmailAndPassword(this.user.username, this.user.password)
     .then(response => {
       console.log(response);
+      loading.dismiss();
+          // Toast melding som sier ifra at du er logget inn og navnet på brukerkontoen.
+          this.toast.create({
+            message: "Du er logget inn som " + this.user.username,
+            duration: 2500 // varer i 2500 ms før den går vekk
+          }).present();
     })
     .catch(error => {
     console.error(error);
+    loading.dismiss();
+    // Feilmelding som sier ifra hva som er feil ved innlogging. Personalisert så du får vite hva som var feil. Bruker alert så brukeren kan ha tid til å lese feilmeldingen og trykke videre på egenhånd
+    let alert = this.alertCtrl.create({
+      title: 'Feil ved innlogging',
+      subTitle: error,
+      buttons: ['Lukk melding']
+    });
+    alert.present();
   })
 }
 
   // Register
   registerUser() {
+    let loading = this.loadingCtrl.create({ content: "Registrerer..."});
+    loading.present();
+
     this.af.app.auth()
       .createUserWithEmailAndPassword(this.user.username, this.user.password)
       .then(response => {
         console.log(response);
+        loading.dismiss();
       })
       .catch(error => {
         console.error(error);
+        loading.dismiss();
+        // Feilmelding som sier ifra hva som er feil ved registering. Personalisert så du får vite hva som var feil.
+        let alert = this.alertCtrl.create({
+          title: 'Feil ved registrering',
+          subTitle: error,
+          buttons: ['Lukk melding']
+        });
+        alert.present();
       })
   }
 
